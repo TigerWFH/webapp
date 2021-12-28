1e24f7e3d146d19a4fd3cce4abec8d1f48af6e56
monkey <monkey>
2021-12-28 19:27:37 +0800

    then过程，thenable延迟一时序；promise延迟两时序；其它直接确定promise对象；

--- mock/demo.js
+++ mock/demo.js
@@ -1,3 +1,44 @@
-console.log('process.cwd=====', process.cwd());
-console.log('__dirname', __dirname);
-console.log('__finename', __filename);
+// console.log('process.cwd=====', process.cwd());
+// console.log('__dirname', __dirname);
+// console.log('__finename', __filename);
+
+// const p = Promise.resolve(4);
+// new Promise((resolve) => resolve(p))
+//   .then(() => {
+//     console.log(0);
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   });
+Promise.resolve()
+  .then(() => {
+    // then的解析过程应该是new Promise()
+    console.log(0);
+    // const p = Promise.resolve(4); // 0,1,2,3,4,5
+    // return p;
+    let thenable = {
+      // 0,1,then,2,4,3,5
+      then: function (resolve) {
+        console.log('then');
+        resolve(4);
+      }
+    };
+
+    return thenable;
+  })
+  .then((res) => {
+    console.log(res);
+  });
+Promise.resolve()
+  .then(() => {
+    console.log(1);
+  })
+  .then(() => {
+    console.log(2);
+  })
+  .then(() => {
+    console.log(3);
+  })
+  .then(() => {
+    console.log(5);
+  });
