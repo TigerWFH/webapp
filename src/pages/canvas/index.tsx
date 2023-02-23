04b6ae54564f95b2d7440c54c927539bdca0c617
monkey <monkey>
2023-02-23 13:30:04 +0800

    feat: 调整canvas布局

--- src/pages/canvas/index.tsx
+++ src/pages/canvas/index.tsx
@@ -1,22 +1,30 @@
-import * as React from "react"
-import Demo1 from "./components/Demo1"
-import Rect from "./components/Rect"
-import Path from "./components/Path"
-import styles from "./index.module.scss"
+import * as React from 'react';
+import Tabs, { TabPanel } from 'Components/Tabs';
+import Demo1 from './components/Demo1';
+import Rect from './components/Rect';
+import Path from './components/Path';
+import styles from './index.module.scss';
 
 export default function Canvas(props: any) {
-    
-    return (
-        <div className={styles.canvas}>
-            <div className={styles.container}>
-                <Path className={styles.component}/>
-            </div>
-            <div className={styles.container}>
-                <Rect className={styles.component}/>
-            </div>
-            <div className={styles.container}>
-                <Demo1 className={styles.component}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.canvas}>
+      <Tabs defaultActiveName="path">
+        <TabPanel key="@@path" name="path" title="path">
+          <div className={styles.container}>
+            <Path className={styles.component} />
+          </div>
+        </TabPanel>
+        <TabPanel key="@@rect" name="rect" title="rect">
+          <div className={styles.container}>
+            <Rect className={styles.component} />
+          </div>
+        </TabPanel>
+        <TabPanel key="@@demo1" name="demo1" title="demo1">
+          <div className={styles.container}>
+            <Demo1 className={styles.component} />
+          </div>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 }
