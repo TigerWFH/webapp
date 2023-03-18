6cdea171ffeff8a15ab9e159ac9ae53d04209f78
monkey <fanghuawfh@gmail.com>
2023-03-18 23:10:08 +0800

    feat: xflow文档

--- src/pages/x6/xflow.md
+++ src/pages/x6/xflow.md
@@ -2,9 +2,142 @@
 
 https://www.yuque.com/antv/x6/thk2z7
 
-## Hook
+## 画布配置：createGraphConfig<https://xflow.antv.vision/docs/tutorial-core-components/xflow-canvas-component>
 
-> 分为两类
+> XFlow 和 XFlowCanvas 都支持配置，优先级未定
+
+- `setX6Config`(options: X6Graph.Options) => void,配置 GraphOptions,x6 画布的配置
+- `setNodeRender`(renderKey: string, component: NsGraphConfig.INodeRender) => void,设置 renderKey 和对应 React 组件
+- `setEdgeRender`(renderKey: string, component: NsGraphConfig.IEdgeRender) => void,设置 renderKey 和对应 React 组件
+- `setNodeTypeParser`(parser: (nodeData: NsGraph.INodeConfig) => string) => void,设置解析 node render key 的 parser
+- `setEdgeTypeParser`(parser: (edgeData: NsGraph.INodeConfig) => string) => void,设置解析 edge render key 的 parser
+- `setEvents`(events: GraphEvent[]) => void,配置 GraphEvents
+
+- `createGraphConfig`配置画布
+
+```js
+import { createGraphConfig } from '@antv/xflow';
+import Node1 from './react-node/node1';
+import Edge1 from './react-edge/edge1';
+
+export const useGraphConfig = createGraphConfig((config) => {
+  /** Setting the canvas configuration item will override the XFlow default canvas configuration item */
+  config.setX6Config({
+    grid: true,
+    scaling: { min: 0.2, max: 3 },
+    mousewheel: { enabled: true, zoomAtMousePosition: true }
+  });
+
+  /** Set the React node that the canvas needs to render, and the React content on the connection */
+  config.setNodeRender('NODE1', (props) => <Node1 {...props} />);
+  config.setEdgeRender('EDGE1', (props) => <Edge1 {...props} />);
+});
+```
+
+## XFlow
+
+> XFlow 工作台组件是 XFlow 的核心组件之一, 可以理解为是一个图编辑应用的工作空间, 它包含了画布组件、各种交互组件等。
+
+> XFlow 支持在工作台初始化之前传入 Meta 元信息, 该元信息会被存储在全局的 ModelService 实例中, 在整个 XFlow 工作空间可用。
+
+> `XFlow 内部会自动进行画布内容 Diff, 相同节点、连线不会重新渲染`
+
+> `XFlow提供了执行命令的钩子, 允许用户提前预设好service层的行为, 在触发某个具体命令时, 会自动调用钩子里的service逻辑。`
+
+> `Flow内置了若干全局状态, 比如画布当前选中的节点/连线、 画布的缩放比例等, 这些全局状态可以在画布中使用、在配套的交互组件中使用, 方便实现画布与交互组件的联动效`
+
+### XFlow 的 props 属性
+
+- `meta:`工作台组件元信息, 会储存在全局 Model 中并在调用 Service 时作为额外的参数传入
+  - `flowId`
+- `graphConfig:`画布的配置，用于配置 X6 的 Graph.Options 和绑定 Graph 的事件
+- `graphData:`画布数据
+- `graphLayout: `布局配置项
+- `onLoad:`可选项，app 初始化成功的回调
+- `isAutoCenter:`画布居中配置
+- `hookConfig:`核心模块钩子函数，可以配置额外的业务逻辑包括以下 4 个 hook。
+  - graphOptions: 在实例化 X6 之前执行
+  - afterGraphInit: 在实例化 X6 后执行
+  - x6Events: 在实例化 X6 后绑定事件
+  - beforeGraphDestroy: 在 X6 实例销毁前执行
+- `modelServiceConfig:`可以在这里扩展工作台全局状态
+- `commandConfig:`在这里配置命令的 hook
+- `onAppDestroy:`xflow app 销毁前的回调
+- `onAppConfigReady:`xflow app 初始化后的回调
+- `style:`app container style
+- `className:`app container classname
+- `xflowPrefixCls:`xflow less 文件中的 prefix 变量
+
+### XFlow 的命令钩子：CommandConfig，Hook 之一
+
+- `createCmdConfig`配置命令
+
+```js
+import { XFlow, DisposableCollection, createCmdConfig } from '@antv/xflow';
+export const useCmdConfig = createCmdConfig((config) => {
+  /** 设置hook */
+  config.setRegisterHookFn((hooks) => {
+    const list = [
+      hooks.addNode.registerHook({
+        name: 'addNodeHook',
+        handler: async (args) => {
+          args.createNodeService = MockApi.addNode;
+        }
+      }),
+      hooks.addEdge.registerHook({
+        name: 'addEdgeHook',
+        handler: async (args) => {
+          args.createEdgeService = MockApi.addEdge;
+        }
+      })
+    ];
+    const toDispose = new DisposableCollection();
+    toDispose.pushAll(list);
+    return toDispose;
+  });
+});
+```
+
+### XFlow 的全局状态钩子：ModelServiceConfig，Hook 之一
+
+- `createModelServiceConfig`配置 model
+
+```js
+import type { IModelService } from '@antv/xflow'
+import { XFlow, createModelServiceConfig } from '@antv/xflow'
+
+export namespace NS_LOADING_STATE {
+  export const id = 'custom-loading'
+  export interface IState {
+    loading: boolean
+  }
+  export const getValue = async (contextService: IModelService) => {
+    const ctx = await contextService.awaitModel<NS_LOADING_STATE.IState>(NS_LOADING_STATE.id)
+    return ctx.getValidValue()
+  }
+}
+
+export const useModelServiceConfig = createModelServiceConfig(config => {
+  config.registerModel(registry => {
+    return registry.registerModel({
+      id: NS_LOADING_STATE.id,
+      getInitialValue: () => {
+        loading: true
+      },
+    })
+  })
+})
+export const Demo = () => {
+  const modelServiceConfig = useModelServiceConfig()
+  return <XFlow modelServiceConfig={modelServiceConfig}></XFlow>
+}
+```
+
+### XFlow 的 Hook
+
+- `createHookConfig`配置 hook
+
+  > 有点代替 createGraphConfig 的样子???
 
 - `GraphHook:`配置 Graph 相关的配置项
   - `graphOptions`x6 graph 配置项
@@ -39,73 +172,10 @@ export const useGraphHookConfig = createHookConfig((config) => {
 - `CommandHook:`配置可以修改 Command 参数的逻辑
   > INodeHooks & IEdgeHooks & IGroupHooks & IGraphHooks & IModelHooks
 
-```js
-export const useCmdConfig = createCmdConfig((config) => {
-  /** 设置hook */
-  config.setRegisterHookFn((hooks) => {
-    const list = [
-      hooks.addNode.registerHook({
-        name: 'addNodeHook',
-        handler: async (args) => {
-          args.createNodeService = MockApi.addNode;
-        }
-      }),
-      hooks.addEdge.registerHook({
-        name: 'addEdgeHook',
-        handler: async (args) => {
-          args.createEdgeService = MockApi.addEdge;
-        }
-      })
-    ];
-    const toDispose = new DisposableCollection();
-    toDispose.pushAll(list);
-    return toDispose;
-  });
-});
-```
+### XFlow 其它
 
-## XFlow
-
-> XFlow 工作台组件是 XFlow 的核心组件之一, 可以理解为是一个图编辑应用的工作空间, 它包含了画布组件、各种交互组件等。
-
-> XFlow 支持在工作台初始化之前传入 Meta 元信息, 该元信息会被存储在全局的 ModelService 实例中, 在整个 XFlow 工作空间可用。
-
-> `XFlow 内部会自动进行画布内容 Diff, 相同节点、连线不会重新渲染`
-
-> `XFlow提供了执行命令的钩子, 允许用户提前预设好service层的行为, 在触发某个具体命令时, 会自动调用钩子里的service逻辑。`
-
-> `Flow内置了若干全局状态, 比如画布当前选中的节点/连线、 画布的缩放比例等, 这些全局状态可以在画布中使用、在配套的交互组件中使用, 方便实现画布与交互组件的联动效`
-
-### props 属性
-
-- `meta:`工作台组件元信息, 会储存在全局 Model 中并在调用 Service 时作为额外的参数传入
-  - `flowId`
-- `graphConfig:`画布的配置，用于配置 X6 的 Graph.Options 和绑定 Graph 的事件
-- `graphData:`画布数据
-- `graphLayout: `布局配置项
-- `onLoad:`可选项，app 初始化成功的回调
-- `isAutoCenter:`画布居中配置
-- `hookConfig:`核心模块钩子函数，可以配置额外的业务逻辑包括以下 4 个 hook。
-  - graphOptions: 在实例化 X6 之前执行
-  - afterGraphInit: 在实例化 X6 后执行
-  - x6Events: 在实例化 X6 后绑定事件
-  - beforeGraphDestroy: 在 X6 实例销毁前执行
-- `modelServiceConfig:`可以在这里扩展工作台全局状态
-- `commandConfig:`在这里配置命令的 hook
-- `onAppDestroy:`xflow app 销毁前的回调
-- `onAppConfigReady:`xflow app 初始化后的回调
-- `style:`app container style
-- `className:`app container classname
-- `xflowPrefixCls:`xflow less 文件中的 prefix 变量
-
-### 实例属性
-
-- `createGraphConfig`画布配置
-- `createCmdConfig`
-- `XFLow App`
-  - `getGraphInstance:`异步？
-  - `getGraphConfig:`异步？
-  - `executeCommand`
+- `onLoad：`回调函数
+- ``
 
 ## XFlowCanvas
 
@@ -133,8 +203,4 @@ export const useCmdConfig = createCmdConfig((config) => {
 
 > React 组件内部使用： 通过 useXFlowApp 来获取 CommandService
 
-> XFlow 组件的配置项中使用：通过函数的参数可以获得 CommandService
-
-## Hook
-
-> createHookConfig: 使用 Hook 扩展 Command 逻辑
+> XFlow 组件的配置项中使用：通过函数的参数可以获得 CommandServic
--- src/pages/x6/xflow.tsx
+++ src/pages/x6/xflow.tsx
@@ -1,49 +1,174 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { AlgoNode } from './xflow/react-node/algo-node';
+import { GroupNode } from './xflow/react-node/group';
+import { DND_RENDER_ID, GROUP_NODE_RENDER_ID } from './xflow/constant';
 import {
   XFlow,
   XFlowCanvas,
-  CanvasSnapline,
-  CanvasNodePortTooltip,
+  // CanvasSnapline,
+  // CanvasNodePortTooltip,
   // NodeCollapsePanel,
   // CanvasScaleToolbar,
   // CanvasContextMenu,
-  DagGraphExtension
+  DagGraphExtension,
+  // createCmdConfig,
+  // createHookConfig,
+  // DisposableCollection,
+  createGraphConfig
 } from '@antv/xflow';
 
-/** app 组件配置  */
-/** 配置画布 */
-// import { useGraphHookConfig } from './xflow/config-graph';
-/** 配置Command */
-// import { useCmdConfig } from './xflow/config-cmd';
-/** 配置Model */
-// import { useModelServiceConfig } from './xflow/config-model-service';
-/** 配置Menu */
-// import { useMenuConfig } from './xflow/config-menu';
-/** 配置Toolbar */
-// import { useToolbarConfig } from './xflow/config-toolbar';
-/** 配置快捷键 */
-// import { useKeybindingConfig } from './xflow/config-keybinding';
-/** 配置Dnd组件面板 */
-// import * as dndPanelConfig from './xflow/config-dnd-panel';
-/** 配置JsonConfigForm */
-/*
-graphHooksConfig: 
-modelServiceConfig:
-commandConfig:
-*/
+const nodes = [
+  {
+    id: 'root1',
+    width: 150,
+    height: 40,
+    renderKey: DND_RENDER_ID,
+    text: '节点1'
+  },
+  {
+    id: 'down1',
+    width: 150,
+    height: 40,
+    renderKey: DND_RENDER_ID,
+    text: '节点2'
+  },
+  {
+    id: 'down2',
+    width: 150,
+    height: 40,
+    renderKey: DND_RENDER_ID,
+    text: '节点3'
+  },
+  {
+    id: 'down3',
+    width: 150,
+    height: 40,
+    renderKey: DND_RENDER_ID,
+    text: '节点4'
+  }
+];
+const edges = [
+  { id: 'root1-down1', source: 'root1', target: 'down1', label: '1:1' },
+  { id: 'root1-down2', source: 'root1', target: 'down2', label: '1:N' },
+  { id: 'root1-down3', source: 'root1', target: 'down3', label: 'N:N' }
+];
+
+const mockGraphData: any = {
+  edges,
+  nodes
+};
+
+// 画布配置
+const useGraphConfig = createGraphConfig((config) => {
+  /** 设置XFlow画布配置项 */
+  config.setX6Config({
+    /** 画布网格 */
+    grid: true,
+    /** 画布缩放等级 */
+    scaling: {
+      min: 0.2,
+      max: 1
+    }
+  });
+
+  /** 设置XFlow画布需要渲染的React节点/边 */
+  config.setNodeRender(DND_RENDER_ID, (props) => <AlgoNode {...props} />);
+  config.setNodeRender(GROUP_NODE_RENDER_ID, GroupNode);
+  // config.setEdgeRender('EDGE1', (props) => <Edge1 {...props} />);
+  // config.setEdgeRender('EDGE2', (props) => <Edge2 {...props} />);
+  // config.setNodeTypeParser((node) => node?.renderKey as string);
+});
+
+// 画布hook
+// const useGraphHookConfig = createHookConfig((config, proxy) => {
+//   const props = proxy.getValue();
+//   console.log('graphHookConfig', props);
+//   config.setRegisterHook((hooks) => {
+//     const disposableList = [
+//       // 注册修改graphOptions配置的钩子
+//       hooks.graphOptions.registerHook({
+//         name: 'custom-x6-options',
+//         after: 'dag-extension-x6-options',
+//         handler: async (options) => {
+//           console.log('graphOptions---hook');
+//           // 画布配置调整
+//           options.grid = false;
+//           options.keyboard = {
+//             enabled: true
+//           };
+//         }
+//       }),
+//       // 注册增加 graph event
+//       hooks.x6Events.registerHook({
+//         name: 'add',
+//         handler: async (events) => {
+//           console.log('x6Events---hook');
+//           // events.push({
+//           //   eventName: 'node:moved',
+//           //   callback: (e, cmds) => {
+//           //     const { node } = e;
+//           //     cmds.executeCommand<NsNodeCmd.MoveNode.IArgs>(
+//           //       XFlowNodeCommands.MOVE_NODE.id,
+//           //       {
+//           //         id: node.id,
+//           //         position: node.getPosition()
+//           //       }
+//           //     );
+//           //   }
+//           // });
+//         }
+//       })
+//       // 注册增加 react Node Render
+//       // hooks.reactNodeRender.registerHook({
+//       //   name: 'add react node',
+//       //   handler: async (renderMap) => {
+//       //     console.log('reactNodeRender---hook');
+//       //     renderMap.set(DND_RENDER_ID, AlgoNode);
+//       //     renderMap.set(GROUP_NODE_RENDER_ID, GroupNode);
+//       //     console.log('reactNodeRender---hook-end');
+//       //   }
+//       // })
+//     ];
+//     const toDispose = new DisposableCollection();
+//     toDispose.pushAll(disposableList);
+//     return toDispose;
+//   });
+// });
+
+// 命令hook
+// const useCmdConfig = createCmdConfig((config) => {
+//   config.setRegisterHookFn((hooks) => {
+//     return hooks.addNode.registerHook({
+//       name: 'get node config data from backend api',
+//       handler: async (args: any) => {
+//         debugger;
+//         console.log('addNode-hook===>', args);
+//       }
+//     });
+//   });
+// });
 
 interface IXflowDemo {
   graphData: any;
 }
 function XflowDemo(props: IXflowDemo) {
-  const appRef = React.useRef(null);
-  // const { meta } = props;
-  // const graphHooksConfig = useGraphHookConfig(props);
-  // const toolbarConfig = useToolbarConfig();
-  // const menuConfig = useMenuConfig();
-  // const cmdConfig = useCmdConfig();
-  // const modelServiceConfig = useModelServiceConfig();
-  // const keybindingConfig = useKeybindingConfig();
+  const appRef = useRef(null);
+  const graphConfig = useGraphConfig(props);
+  const { graphData } = props;
+  const [renderGraphData, setRenderGraphData] = useState(graphData);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setRenderGraphData(mockGraphData);
+      console.log('renderGRaph====>', mockGraphData);
+    }, 1000);
+
+    console.log('useEffect6666666');
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   const onLoad = async (app: any) => {
     appRef.current = app;
@@ -53,16 +178,26 @@ function XflowDemo(props: IXflowDemo) {
     <div>
       <XFlow
         className="dag-user-custom-clz"
-        // hookConfig={graphHooksConfig}
-        // modelServiceConfig={modelServiceConfig}
-        // commandConfig={cmdConfig}
+        graphData={renderGraphData}
+        graphLayout={{
+          layoutType: 'dagre',
+          layoutOptions: {
+            type: 'dagre',
+            rankdir: 'TB',
+            nodesep: 60,
+            ranksep: 40
+          }
+        }}
+        isAutoCenter={true}
         onLoad={onLoad}>
         <DagGraphExtension />
-        <XFlowCanvas position={{ top: 40, left: 230, right: 290, bottom: 0 }}>
+        <XFlowCanvas
+          position={{ top: 40, left: 230, right: 290, bottom: 0 }}
+          config={graphConfig as any}>
           {/* <CanvasScaleToolbar position={{ top: 12, right: 12 }} /> */}
           {/* <CanvasContextMenu config={menuConfig} /> */}
-          <CanvasSnapline color="#faad14" />
-          <CanvasNodePortTooltip />
+          {/* <CanvasSnapline color="#faad14" /> */}
+          {/* <CanvasNodePortTooltip /> */}
         </XFlowCanvas>
       </XFlow>
     </div>
